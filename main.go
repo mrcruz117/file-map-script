640247ae2bda3703a8ebcece918eef49d8b30b19
mrcruz117 <mrcruz1989@gmail.com>
2024-11-01 21:47:48 -0400

    initial write

--- main.go
+++ main.go
@@ -1,9 +1,87 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
+var ErrFileFound = errors.New("file found")
+
 func main() {
-	fmt.Println("Hello, World!")
+	csvPath := `C:\Users\mamores\Desktop\PowerShell_POC\mapping.csv`
+	rootDirectory := `C:\Users\mamores\Desktop\PowerShell_POC\MainFolder`
+
+	csvFile, err := os.Open(csvPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer csvFile.Close()
+
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	reader.FieldsPerRecord = -1
+
+	// Read the header row to get column indexes
+	headers, err := reader.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var fileNameIndex, directoryIndex int
+	for i, h := range headers {
+		switch strings.ToLower(h) {
+		case "filename":
+			fileNameIndex = i
+		case "directory":
+			directoryIndex = i
+		}
+	}
+
+	// Process each row in the CSV file
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fileName := record[fileNameIndex]
+		destinationDirectory := record[directoryIndex]
+
+		// Search for the file in the root directory and its subdirectories
+		var filePath string
+		err = filepath.Walk(rootDirectory, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !info.IsDir() && strings.EqualFold(info.Name(), fileName) {
+				filePath = path
+				return ErrFileFound // Signal to stop walking
+			}
+			return nil
+		})
+		if err != nil && err != ErrFileFound {
+			log.Fatal(err)
+		}
+
+		// If the file is found, move it to the destination directory
+		if filePath != "" {
+			destinationPath := filepath.Join(destinationDirectory, fileName)
+			err = moveFile(filePath, destinationPath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Moved '%s' to '%s'\n", fileName, destinationDirectory)
+		} else {
+			fmt.Printf("File '%s' not found in '%s'\n", fileName, rootDirectory)
+		}
+	}
 }
---
+++ move_file.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// moveFile moves a file from sourcePath to destinationPath, overwriting if necessary
+func moveFile(sourcePath, destinationPath string) error {
+	// Remove the destination file if it exists
+	if _, err := os.Stat(destinationPath); err == nil {
+		err = os.Remove(destinationPath)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Create the destination directory if it doesn't exist
+	destDir := filepath.Dir(destinationPath)
+	if _, err := os.Stat(destDir); os.IsNotExist(err) {
+		err = os.MkdirAll(destDir, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Open the source file
+	sourceFile, err := os.Open(sourcePath)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	// Create the destination file
+	destinationFile, err := os.Create(destinationPath)
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+
+	// Copy the file contents
+	_, err = io.Copy(destinationFile, sourceFile)
+	if err != nil {
+		return err
+	}
+
+	// Remove the source file
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
